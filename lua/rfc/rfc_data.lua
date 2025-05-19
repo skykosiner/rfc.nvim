9f0e7a3cd7a39c12a2cce246f2563f1df78c8623
Sky Kosiner <sky@skykosiner.com>
2025-05-19 13:00:50 +0100

    feat: error if no RFC is found with that title

--- lua/rfc/rfc_data.lua
+++ lua/rfc/rfc_data.lua
@@ -1,14 +1,20 @@
-local curl = require "plenary.curl"
-local utils         = require('rfc.utils')
+local curl  = require "plenary.curl"
+local utils = require('rfc.utils')
 
-local M    = {}
+local M     = {}
 
 ---@param query string
 ---@return RFC[]
 function M:run_search(query)
     local res = curl.get("https://datatracker.ietf.org/api/v1/doc/document/?title__contains=" ..
         query:gsub("%s", "%%20") .. "&type=rfc")
-    return vim.fn.json_decode(res.body).objects
+    local json = vim.fn.json_decode(res.body).objects
+
+    if next(json) == nil then
+        error("No RFC found with that title")
+    end
+
+    return json
 end
 
 ---@param rfc_id number
